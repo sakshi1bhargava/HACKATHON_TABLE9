dc413602a0d66260a4df87dd11597a616de42290
Pavan Jalandar <pavanjalandar22@gmail.com>
2019-08-10 12:56:44 +0530

    created contoller and repository

--- src/main/java/com/dbs/repository/DBSMessageQueueRepository.java
+++ src/main/java/com/dbs/repository/DBSMessageQueueRepository.java
@@ -2,7 +2,6 @@ package com.dbs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.dbs.model.MessageQueue;
 
 @Repository
