4ff53dac57a67c32defd9b1458c2e9609a7deb69
Manaswini <Manaswini.challa@aa.com>
2019-08-10 10:58:22 +0530

    Initial Commit

---
+++ src/main/java/com/dbs/queue/Configuration/QueueApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.queue.Configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueApplication.class, args);
+	}
+
+}
