ea16d7cb9df19e5a9721b6ed42aa9a14f4fe7613
Manaswini <Manaswini.challa@aa.com>
2019-08-10 12:23:52 +0530

    Added Model Objects

--- src/main/java/com/dbs/model/MessageQueue.java
+++ src/main/java/com/dbs/model/MessageQueue.java
@@ -1,5 +1,62 @@
 package com.dbs.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema="dbs_queue",name="qmessages")
+@NamedQuery(name="MessageQueue.findAll", query="select msg from MessageQueue msg")
 public class MessageQueue {
+	
+	@Id @GeneratedValue
+	@Column(name="msg_id")
+	private Integer msgId;
+	
+    @Column(name = "qid")
+	private Integer qId;
+	
+	@Column(name="msg")
+	private String msg;
+	
+	@Column(name="crte_time")
+	private Timestamp createTime;
+
+	public Integer getMsgId() {
+		return msgId;
+	}
+
+	public void setMsgId(Integer msgId) {
+		this.msgId = msgId;
+	}
+
+	public Integer getqId() {
+		return qId;
+	}
+
+	public void setqId(Integer qId) {
+		this.qId = qId;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
 
 }
--- src/main/java/com/dbs/model/Queue.java
+++ src/main/java/com/dbs/model/Queue.java
@@ -1,8 +1,66 @@
 package com.dbs.model;
 
+import java.io.Serializable;
+import java.util.List;
 
-public class Queue {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(schema="dbs_queue",name="queues")
+@NamedQuery(name="Queue.findAll", query="select q from Queue q")
+public class Queue implements Serializable{
+
+	/**
+	 *  Generated Serial ID for Queue
+	 */
+	private static final long serialVersionUID = 3010705346235174955L;
+
+	@Id @GeneratedValue
+	@Column(name="qid")
+	private Integer qID;
 	
-	private Integer QID;
+	@Column(name="qname")
+	private String qName;
+	
+	@OneToMany
+    @JoinColumn(name = "qid")
+	private List<MessageQueue> msgQueueLst;
+
+
+	public List<MessageQueue> getMsgQueueLst() {
+		return msgQueueLst;
+	}
+
+	public void setMsgQueueLst(List<MessageQueue> msgQueueLst) {
+		this.msgQueueLst = msgQueueLst;
+	}
+
+	public Integer getQID() {
+		return qID;
+	}
+
+	public void setQID(Integer qID) {
+		this.qID = qID;
+	}
+
+	public String getqName() {
+		return qName;
+	}
+
+	public void setqName(String qName) {
+		this.qName = qName;
+	}
+	
+	@Override
+	public String toString(){
+		return "Queue["+qName+"] with ID "+qID;
+	}
+	
 }
