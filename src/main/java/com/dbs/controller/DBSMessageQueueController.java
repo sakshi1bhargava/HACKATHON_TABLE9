d6f3ab86742dc5dd0102a1e0cd742d5802515f94
Mohan <Mohan@IronMan.mshome.net>
2019-08-10 12:54:03 +0530

    Created skeleton for messageQueue controller and repository

---
+++ src/main/java/com/dbs/controller/DBSMessageQueueController.java
@@ -0,0 +1,10 @@
+package com.dbs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/messages")
+public class DBSMessageQueueController{
+
+}
